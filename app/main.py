891cb30fbb75d5b5bf8ff751cc5a6a9105d132c6
Ruadgedy <596318128@qq.com>
2025-10-09 23:43:03 +0800

    Handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,25 +1,21 @@
 import socket  # noqa: F401
+import threading
+
+BUFFER_SIZE = 2048
+
+def handle_command(client: socket.socket):
+    while chunk := client.recv(BUFFER_SIZE):
+        if chunk == b"":
+            break
+        print(f"Received client msg: {chunk.decode()}\n")
+        client.send(b"+PONG\r\n")
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn,addr = server_socket.accept() # wait for client
-    print(f"Connected to server! Address:{addr}")
-
     while True:
-        b = conn.recv(2048)
-        st = b.decode().strip().split("\n")
-        for s in st:
-            print(s)
-            if s == "PING":
-                print("sending pong")
-                conn.sendall(b"+PONG\r\n")
-        # conn.sendall(b"+Chat End\r\n")
-    conn.close()
+        conn, addr = server_socket.accept()  # wait for client
+        print(f"Connected by: {addr}\n")
+        threading.Thread(target=handle_command, args=(conn,)).start()
 
 if __name__ == "__main__":
     main()
