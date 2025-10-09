0f76f62474b431cee797baa44d45ed5fdfe2f7de
Ruadgedy <596318128@qq.com>
2025-10-09 22:33:37 +0800

    response to ping

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import socket  # noqa: F401
 
-
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -8,7 +7,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn,addr = server_socket.accept() # wait for client
+    print(f"Connected to server! Address:{addr}")
+
+    conn.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
