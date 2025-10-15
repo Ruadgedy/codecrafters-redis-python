147ae782170104a9386672d97e8e0dccf60b8101
Ruadgedy <596318128@qq.com>
2025-10-16 00:14:38 +0800

    echo command

--- app/main.py
+++ app/main.py
@@ -14,13 +14,13 @@ def handle_command(client: socket.socket):
 
         parse = parse_resp(chunk.decode('utf-8'))
         if isinstance(parse, list): # Arrays
+            if len(parse) == 1 and parse[0].lower() == "ping":
+                client.send(b"+PONG\r\n")
             if len(parse) == 2 and parse[0].lower() == "echo" :
                 content = parse[1]
                 content_bytes = content.encode('utf-8')
                 resp_str = f'${len(content_bytes)}\r\n{content}\r\n' # ECHO command
                 client.sendall(resp_str.encode('utf-8'))
-        elif isinstance(parse, str) and parse.lower() == "ping" :
-            client.send(b"+PONG\r\n")
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
